ed26b2d25a34da8fc7e1af415ec7ceada0a729d6
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-10-03 16:23:01 +0200

    How to use styled-components and CSS in React Native (we should use a React Native elements instead of HTML and also we should import styled-components from styled-components/native

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const Post = styled.View`
+  padding: 15px;
+  height: 100px;
+  width: 100px;
+  background-color: red;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.testText}>Hello World111!!!!!!!!!!!!!!!!!!!!!!!</Text>
       <Text style={{ fontSize: 30, color: "blue" }}>Message</Text>
+      <Post />
     </View>
   );
 }
