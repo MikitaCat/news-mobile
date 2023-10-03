1876c7d627143dcbf36ae85d4c9f561356aa6f56
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-10-03 17:18:58 +0200

    Decomposition, components directory has been added

--- App.js
+++ App.js
@@ -1,47 +1,10 @@
 import { StatusBar, View, Image } from "react-native";
-import styled from "styled-components/native";
-
-const Post = styled.View`
-  flex-direction: row;
-  padding: 15px;
-  border-bottom-width: 1px;
-  border-bottom-color: rgba(0, 0, 0, 0.1);
-  border-bottom-style: solid;
-`;
-
-const PostImage = styled.Image`
-  width: 60px;
-  height: 60px;
-  border-radius: 12px;
-  margin-right: 12px;
-`;
-
-const PostTitle = styled.Text`
-  font-size: 17px;
-  font-weight: 700;
-`;
-
-const PostDetails = styled.View`
-  flex: 1;
-  justify-content: center;
-`;
-
-const PostDate = styled.Text`
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.4);
-  margin-top: 2px;
-`;
+import Post from "./components/Post";
 
 export default function App() {
   return (
     <View>
-      <Post>
-        <PostImage source={{ uri: "https://picsum.photos/200/300" }} />
-        <PostDetails>
-          <PostTitle>Test Article</PostTitle>
-          <PostDate>03/10/2023</PostDate>
-        </PostDetails>
-      </Post>
+      <Post />
       <StatusBar theme="auto" />
     </View>
   );
---
+++ components/Post.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components/native";
+
+const PostView = styled.View`
+  flex-direction: row;
+  padding: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0, 0.1);
+  border-bottom-style: solid;
+`;
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const PostTitle = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+`;
+
+const PostDetails = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const PostDate = styled.Text`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`;
+
+export default function Post() {
+  return (
+    <PostView>
+      <PostImage source={{ uri: "https://picsum.photos/200/300" }} />
+      <PostDetails>
+        <PostTitle>Test Article</PostTitle>
+        <PostDate>03/10/2023</PostDate>
+      </PostDetails>
+    </PostView>
+  );
+}
