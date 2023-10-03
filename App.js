5bd8e8ef139dfe3f6e3b548438db6671b3038fd3
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-10-03 21:40:02 +0200

    I made a preloader using ActivityIndicator component from React Native

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import { FlatList, StatusBar, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  StatusBar,
+  View,
+  Text,
+} from "react-native";
 import Post from "./components/Post";
 import { useEffect, useState } from "react";
 import { getPosts } from "./API/getPosts";
 
 export default function App() {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getPosts()
       .then((data) => {
         setNews(data);
-        console.log("DATA", news);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={40} color="#0000ff" />
+        <Text style={{ marginTop: 15 }}>Loading</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
