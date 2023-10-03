d2aabf1d1fb18e0cad209a214cb270e3d0e1b8c9
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-10-03 23:45:53 +0200

    FINAL!!!!!! In this commit I've implemented the function that allows to make the title shorter, and also I changed the date format using Date().toLocaleDateString(). Yeah, I don't like the project architecture, but it is just a project for learning React Native and Expo. It was funny. And now I will go to sleep)))

--- components/Post.jsx
+++ components/Post.jsx
@@ -31,13 +31,21 @@ const PostDate = styled.Text`
   margin-top: 2px;
 `;
 
+const truncateTitle = (str) => {
+  if (str.length >= 50) {
+    return str.substr(0, 50) + "...";
+  }
+
+  return str;
+};
+
 export default function Post({ imageUrl, title, createdAt }) {
   return (
     <PostView>
       <PostImage source={{ uri: imageUrl }} />
       <PostDetails>
-        <PostTitle>{title}</PostTitle>
-        <PostDate>{createdAt}</PostDate>
+        <PostTitle>{truncateTitle(title)}</PostTitle>
+        <PostDate>{new Date(createdAt).toLocaleDateString()}</PostDate>
       </PostDetails>
     </PostView>
   );
